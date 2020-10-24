5d824a190445ab3fac112d2926c5a090c64889ec
Alexandre de Souza <alexandre@aledsz.com.br>
2020-10-24 11:24:40 -0300

    Fix ESLint issues

--- src/commands/jumpToTest.js
+++ src/commands/jumpToTest.js
@@ -66,25 +66,27 @@ function handler() {
     return;
   }
 
-  const startDir = openedFile[1]
-  const testOrLib = openedFile[2]
-  const postDir = openedFile[3]
-  const fileName = openedFile[4]
-  const replacedLibOrTest = testOrLib === 'lib' ? 'test': 'lib';
+  const startDir = openedFile[1];
+  const testOrLib = openedFile[2];
+  const postDir = openedFile[3];
+  const fileName = openedFile[4];
+  const replacedLibOrTest = testOrLib === 'lib' ? 'test' : 'lib';
+  let newFilename = '';
 
   if (fileName.includes('_test')) {
     const strippedFileName = fileName.replace('_test', '');
     newFilename = `${strippedFileName}.ex`;
-  }
-  else{
-    newFilename = `${fileName}_test.exs`
+  } else {
+    newFilename = `${fileName}_test.exs`;
   }
 
-  const fileToOpen = vscode.workspace.asRelativePath(startDir+replacedLibOrTest+postDir+newFilename)
+  const fileToOpen = vscode.workspace.asRelativePath(
+    startDir + replacedLibOrTest + postDir + newFilename,
+  );
 
   vscode.workspace.findFiles(fileToOpen, '**/.elixir_ls/**').then((files) => {
     if (!files.length) {
-      askToCreateANewFile(startDir+replacedLibOrTest+postDir, newFilename);
+      askToCreateANewFile(startDir + replacedLibOrTest + postDir, newFilename);
     } else {
       const file = files[0].fsPath;
       openFile(file);
